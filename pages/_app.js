33eee5b87a5970866f1afba707e224f6970197c6
Adan <999a.d.a.n.a.y.a.z999@gmail.com>
2023-04-28 18:15:32 +0500

    potential fix css failed imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import localFont from "next/font/local";
 import dynamic from "next/dynamic";
 import "@/styles/globals.css";
 
@@ -5,9 +6,20 @@ const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
   ssr: false,
 });
 
+const nextFontMona = localFont({
+  src: "../assets/fonts/Mona Sans/Mona-Sans-SemiBold.ttf",
+});
+
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <style jsx global>
+        {`
+          h1 {
+            font-family: ${nextFontMona.style.fontFamily};
+          }
+        `}
+      </style>
       <AnimatedCursor
         innerScale={4}
         innerSize={0}
